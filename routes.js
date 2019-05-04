2b0fdaf1b55d3ad31a9d867f5f0b51e8d6ca2ddc
Soonwoo Hong <victor90.h@gmail.com>
2019-05-04 15:58:35 +0200

    10.1

--- routes.js
+++ routes.js
@@ -31,6 +31,11 @@ const FB_CALLBACK = "/auth/facebook/callback";
 const GITHUB = "/auth/github";
 const GITHUB_CALLBACK = "/auth/github/callback";
 
+// API
+
+const API = "/api";
+const REGISTER_VIEW = "/:id/view";
+
 const routes = {
   home: HOME,
   join: JOIN,
@@ -74,7 +79,9 @@ const routes = {
   githubCallback: GITHUB_CALLBACK,
   me: ME,
   facebook: FB,
-  facebookCallback: FB_CALLBACK
+  facebookCallback: FB_CALLBACK,
+  api: API,
+  registerView: REGISTER_VIEW
 };
 
 export default routes;
